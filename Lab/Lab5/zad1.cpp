b9ce4e2aed3fbaf1eecd226ce466d598fad4faee
Lunja <68188529+lunjae@users.noreply.github.com>
2026-06-16 23:44:56 +0200

    Lab5

---
+++ Lab/Lab5/zad1.cpp
@@ -0,0 +1,12 @@
+﻿#include "zad1.h"
+#include <stdlib.h>
+#include <math.h>
+#include "omp.h"
+#include <stdio.h>
+
+#define N 256
+
+int main(){
+    double (*A)[N] = malloc(sizeof(double[N][N]));
+    
+}
